5064bea61573f49204f114a5de52b908e6630013
mkag-auto <michaeltdwt@gmail.com>
2026-03-11 12:04:55 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BLOB_READ_WRITE_TOKEN: process.env.PBLOB_READ_WRITE_TOKEN,
+  },
 };
 
 module.exports = nextConfig;
